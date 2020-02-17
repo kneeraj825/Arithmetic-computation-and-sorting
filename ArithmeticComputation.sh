ce0fb0fa1fb2b7fce9a95a78841b34fe17313801
kneeraj825 <kneeraj825@gmail.com>
2020-02-17 10:49:44 +0530

    Added:Taking three input from user

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,2 +1,7 @@
 #!/bin/bash -x
 echo Welcome
+ 
+#taking input from user
+read -p "Enter the value of a:" firstNumber
+read -p "Enter the value of b:" secondNumber
+read -p "Enter the value of c:" thirdNumber
