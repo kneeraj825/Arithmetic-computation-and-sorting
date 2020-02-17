5936b8ebeaed280c6f1cc301093939ffae387a32
kneeraj825 <kneeraj825@gmail.com>
2020-02-17 10:55:12 +0530

    Added:First Arithmetic operation

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -5,3 +5,6 @@ echo Welcome
 read -p "Enter the value of a:" firstNumber
 read -p "Enter the value of b:" secondNumber
 read -p "Enter the value of c:" thirdNumber
+#performing Arithmetic operations on numbers
+firstOperation=$(( $firstNumber+$secondNumber*$thirdNumber))
+echo "First result is:$firstOperation"
