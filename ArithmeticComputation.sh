6d30731d1b9d544156ef8c0ee9f182177c3c6ae0
kneeraj825 <kneeraj825@gmail.com>
2020-02-17 11:47:42 +0530

    Added:Storing dictionary value into array

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -6,19 +6,26 @@ read -p "Enter the value of a:" firstNumber
 read -p "Enter the value of b:" secondNumber
 read -p "Enter the value of c:" thirdNumber
 #performing  Arithmetic operations on numbers
-firstOperation=$(( $firstNumber+$secondNumber*$thirdNumber))
-echo "First result is:$firstOperation"
-secondOperation=$(( $firstNumber*$secondnumber+$thirdNumber))
-echo "Second result is:$secondOperation"
-thirdOperation=`echo "scale=2;$thirdNumber+$firstNumber/$secondNumber" | bc`
-echo "Third result is:$thirdOperation"
-fourthOperation=`echo "scale=2; $firstNumber%$secondNumber+$thirdNumber" | bc`
-echo "Fourth result is:$fourthOperation"
+operation1=$(( $firstNumber+$secondNumber*$thirdNumber))
+echo "First result is:$operation1"
+operation2=$(( $firstNumber*$secondnumber+$thirdNumber))
+echo "Second result is:$operation2"
+operation3=`echo "scale=2;$thirdNumber+$firstNumber/$secondNumber" | bc`
+echo "Third result is:$operation3"
+operation4=`echo "scale=2; $firstNumber%$secondNumber+$thirdNumber" | bc`
+echo "Fourth result is:$operation4"
 
 #STORING OPERATIONS IN DICTIONARY
 declare -A arithmetic
-arithmetic[firstOperation]=$firstOperation
-arithmetic[secondOperation]=$secondOperation
-arithmetic[thirdOperation]=$thirdOperation
-arithmetic[fourthOperation]=$fourthOperation
+arithmetic[operation1]=$operation1
+arithmetic[operation2]=$operation2
+arithmetic[operation3]=$operation3
+arithmetic[operation4]=$operation4
 echo ${arithmetic[@]}
+
+#storing Dictionary into array
+for (( index=0; index<${#arithmetic[@]}; index++ ))
+do
+   array[index]=${arithmetic[operation$((index+1))]}
+done
+echo ${array[@]}
